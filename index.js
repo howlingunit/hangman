e28ab30ec063d801c56ba829eb8077e1eea99689
howlingunit <tyler_pitt@outlook.com>
2021-12-03 11:23:52 +0000

    refactoring and quality of code fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let score = 0;
-let GUnderscours = [];
+let Gunderscores = [];
 let win = false;
 let usedLetters = [];
 
@@ -14,49 +14,35 @@ function Pickword(){
     const wordList = ["delay","correct","lick","whine","paste","grab","rejoice","work","manage","scatter","dare","cry","queue","type","increase","arrange","trust","order","report","invite","search","fail","scare","disagree","tame","start","spell","expect","pause","separate","walk","hug","prevent","recognise","inform","shade","drip","consider","pass","flood","bow","pat","introduce","tap","approve","worry","coil","contain","suspect","agree","improve","park","obtain","produce","develop","surprise","saw","reproduce","need","point","colour","clear","bleach","suppose","satisfy","intend","untidy","reduce","laugh","lighten","bubble","handle","alert","stamp","suffer","deserve","jump","slap","hop","switch","mark","behave","juggle","unpack","scorch","grin","end","hover","serve","wish","arrest","scream","compare","blind","wave","tie","provide","slow","rhyme","admit"];
     // const wordList = ["test", "test2", "test3", "test4", "and", "so", "on"];
     const word = wordList[Math.floor(Math.random() * wordList.length)];
-    const [Underscours, completeWord] = makeWordArr(word);
-    return [Underscours, completeWord];
+    const [underscores, completeWord] = makeWordArr(word);
+    return [underscores, completeWord];
 }
 
 function makeWordArr(word){
-    let Underscours = [];
+    let underscores = [];
     let completeWord = [];
     for(const item of word){
-        Underscours.push("_");
+        underscores.push("_");
         completeWord.push(item);
     }
-    return [Underscours, completeWord];
+    return [underscores, completeWord];
 }
 
-function winf(completeWord){
-    //will add later
-    const endPage = document.querySelector("#end");
-    const playPage = document.querySelector("#playing");
-    const endInfoText = document.querySelector("#endInfoText");
-    // console.log("win");
-    endPage.classList.remove("invis");
-    playPage.classList.add("invis");
-    endInfoText.textContent = `You Win with only ${8-score} lives left, the word was ${completeWord.join("")}`;
-}
-
-function lose(completeWord){
+function end(completeWord){
     const endPage = document.querySelector("#end");
     const playPage = document.querySelector("#playing");
     const endInfoText = document.querySelector("#endInfoText");
     const Res = document.querySelector("#EndRes");
     endPage.classList.remove("invis");
     playPage.classList.add("invis");
-    Res.textContent = `You Loose`
-    endInfoText.textContent = `You lost, the word is ${completeWord.join("")}`;
-
-}
-
-function checkWin(completeWord){
-    if(completeWord.join() === GUnderscours.join()){
-        return true;
+    if (win === true){
+        resizeBy.textContent = "You win!"
+        endInfoText.textContent = `You Win with only ${8-score} lives left, the word was ${completeWord.join("")}`;
     } else{
-        return false;
+        Res.textContent = `You Loose`
+        endInfoText.textContent = `You lost, the word is ${completeWord.join("")}`;
     }
+
 }
 
 function turn(completeWord, e){
@@ -73,44 +59,43 @@ function turn(completeWord, e){
         usedLetters.push(letter);
         let place = 0;
         const OldUnderscour = [];
-        for(const item of GUnderscours){
+        for(const item of Gunderscores){
             OldUnderscour.push(item);
         }
         for (const wordLetter of completeWord){
             if (wordLetter === letter){
-                GUnderscours.splice(place, 1, letter);
+                Gunderscores.splice(place, 1, letter);
             }
             place++;
         }
-        if (OldUnderscour.join() === GUnderscours.join()){
+        if (OldUnderscour.join() === Gunderscores.join()){
             score +=1;
         }
     }
 
     //end of turn
     infoText.textContent = `you have ${8 - score} goes left, your used letters are: ${usedLetters.join(", ")}`; 
-    wordBox.textContent = GUnderscours.join(" ");
+    wordBox.textContent = Gunderscores.join(" ");
     HangPic.src = `assets/${score}.png`;
     document.querySelector("#letter").value = "";
 
-    win = checkWin(completeWord);
+    win = completeWord.join() === Gunderscores.join();
     if(win === true){
-        winf(completeWord);
+        end(completeWord);
     } else if (score >= 8){
-        lose(completeWord);
+        end(completeWord);
     }
 }
 
-function onLoad(){
-
+function init(){
     const endPage = document.querySelector("#end");
     const playPage = document.querySelector("#playing");
     const replayButton = document.querySelector("#replay");
     const HangPic = document.querySelector("#HangPic");
     const infoText = document.querySelector("#infoText");
     const subButton = document.querySelector("#submit");
-    let [Underscours, completeWord] = Pickword();
-    GUnderscours = Underscours;
+    let [underscores, completeWord] = Pickword();
+    Gunderscores = underscores;
     usedLetters = [];
     score = 0;
 
@@ -119,12 +104,12 @@ function onLoad(){
 
     
     infoText.textContent = ` you have ${8 - score} goes left, your used letters are: ${usedLetters.join(", ")}`; 
-    wordBox.textContent = GUnderscours.join(" ");
+    wordBox.textContent = Gunderscores.join(" ");
     HangPic.src = `assets/${score}.png`;
     subButton.addEventListener("click", turn.bind(null, completeWord));
     document.addEventListener("keyup", function(event) {
-        // console.log(event.keyCode);
-        if(event.keyCode === 13){
+        console.log(event.code);
+        if(event.code === "Enter"){
             event.preventDefault();
             turn(completeWord, null);
         }
@@ -132,3 +117,4 @@ function onLoad(){
     replayButton.addEventListener("click", refresh);
 
 }
+window.addEventListener("load", init);
