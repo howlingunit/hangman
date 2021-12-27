1ce85d7444948065a34809091b77efea063cb1a3
howlingunit <tyler_pitt@outlook.com>
2021-12-27 16:43:46 +0000

    added spaces to game and fixed typo

--- hangman.js
+++ hangman.js
@@ -5,6 +5,7 @@ let lives;
 let win;
 let usedLetters;
 let underscores;
+let userWord;
 let completeWord;
 
 //other functions///////////////////////////////////////////////////////////////////
@@ -32,6 +33,7 @@ function fetchWords(){
 function fetchArray(name){
     //this is a temp solution before server stuff and fetch
     const movies = ["jurassic park","lord of the rings ","mission imposible","anchorman","groundhog day","fantastic mr fox","harry potter","shrek","how to train your drangon","jumanji","inception","the matrix","interstellar","batman the dark night","tenet","john wick","back to the future","godzilla vs kong","the shining","the terminal","cast away","labyrinth","hot fuzz","shaun of the dead","worlds end","spirited away","bee movie"];
+    const movies = ["jurassic park","lord of the rings ","mission imposible","anchorman","groundhog day","fantastic mr fox","harry potter","shrek","how to train your dragon","jumanji","inception","the matrix","interstellar","batman the dark night","tenet","john wick","back to the future","godzilla vs kong","the shining","the terminal","cast away","labyrinth","hot fuzz","shaun of the dead","worlds end","spirited away","bee movie"];
     const randomWords = ["news","requirement","formal","manage","depart","definite","ridge","content","fragment","integrity","humanity","jazz","hangman","confuse","sunrise"];
     const TVShows = ["stranger things","friends","how i met your mother","the mandalorian","sherlock","the umbrella acadamy","community","the good place","the it crowd","dirk gentlys holistic detective agency","queens gambit","star trek","black mirror","the office","brooklyn nine nine","avatar the last air bender","santa clarita diet"];
     if(name === "movies"){
@@ -50,7 +52,7 @@ function init(){
 }
 
 function startGame(){
-    [underscores, completeWord] = Pickword();
+    [underscores, completeWord, userWord] = Pickword();
     setLives();
     infoText();
     makeEventListeners();
@@ -104,18 +106,25 @@ function Pickword(){
     // const wordList = ["delay","correct","lick","whine","paste","grab","rejoice","work","manage","scatter","dare","cry","queue","type","increase","arrange","trust","order","report","invite","search","fail","scare","disagree","tame","start","spell","expect","pause","separate","walk","hug","prevent","recognise","inform","shade","drip","consider","pass","flood","bow","pat","introduce","tap","approve","worry","coil","contain","suspect","agree","improve","park","obtain","produce","develop","surprise","saw","reproduce","need","point","colour","clear","bleach","suppose","satisfy","intend","untidy","reduce","laugh","lighten","bubble","handle","alert","stamp","suffer","deserve","jump","slap","hop","switch","mark","behave","juggle","unpack","scorch","grin","end","hover","serve","wish","arrest","scream","compare","blind","wave","tie","provide","slow","rhyme","admit"];
     // const wordList = ["test", "test2", "test3", "test4", "and", "so", "on"];
     const word = wordList[Math.floor(Math.random() * wordList.length)];
-    const [underscores, completeWord] = makeWordArr(word);
-    return [underscores, completeWord];
+    const [underscores, completeWord, userWord] = makeWordArr(word);
+    return [underscores, completeWord, userWord];
 }
 
 function makeWordArr(word){
-    let underscores = [];
-    let completeWord = [];
+    underscores = [];
+    completeWord = [];
+    userWord = [];
     for(const item of word){
-        underscores.push("_");
+        if (item === " "){
+            underscores.push(",");
+            userWord.push(",");
+        } else {
+            underscores.push("_");
+            userWord.push(item);
+        }
         completeWord.push(item);
     }
-    return [underscores, completeWord];
+    return [underscores, completeWord, userWord];
 }
 
 function setLives(){
@@ -153,7 +162,7 @@ function turn(){
         for(const item of underscores){
             OldUnderscour.push(item);
         }
-        for (const wordLetter of completeWord){
+        for (const wordLetter of userWord){
             if (wordLetter === letter){
                 underscores.splice(place, 1, letter);
             }
@@ -167,7 +176,7 @@ function turn(){
     //end of turn
     infoText()
 
-    win = completeWord.join() === underscores.join();
+    win = userWord.join() === underscores.join();
     if(win === true){
         end();
     } else if (livesCounter <= 0){
