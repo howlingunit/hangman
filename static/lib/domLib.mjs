91dc3fd4c334e1b02f88448927f1228d1cd2e613
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-03-04 13:13:33 +0000

    fully auto linted

--- static/lib/domLib.mjs
+++ static/lib/domLib.mjs
@@ -1,95 +1,92 @@
-export function navBarSetup(){
-    const maxWidth = window.matchMedia("(max-width: 600px)");
-    const burgerButton = document.querySelector(".burgerBtn");
+export function navBarSetup() {
+  const maxWidth = window.matchMedia('(max-width: 600px)');
+  const burgerButton = document.querySelector('.burgerBtn');
 
-    if (maxWidth.matches){
-        burgerButton.addEventListener("click", navBar)
-    }
+  if (maxWidth.matches) {
+    burgerButton.addEventListener('click', navBar);
+  }
 }
 
-function navBar(){
-    const nav = document.querySelector(".small");
-    const navLinks = document.querySelector(".navLinks");
+function navBar() {
+  const nav = document.querySelector('.small');
+  const navLinks = document.querySelector('.navLinks');
 
-    if(navLinks.style.display == "none"){
-        nav.style.height = "13rem"
-        navLinks.style.display = "grid"
-        return;
-    }
-    nav.style.height = "4rem"
-    navLinks.style.display = "none";
+  if (navLinks.style.display === 'none') {
+    nav.style.height = '13rem';
+    navLinks.style.display = 'grid';
+    return;
+  }
+  nav.style.height = '4rem';
+  navLinks.style.display = 'none';
 }
 
-export function createKeyboard(){
-    const keys = ["nl","q","w","e","r","t","y","u","i","o","p","nl","a","s","d","f","g","h","j","k","l","nl","z","x","c","v","b","n","m"]
-    const keyboardDiv = document.querySelector("#keyboard");
-    let currentLine
-    for(let i = 0; i < keys.length; i++){
-        if(keys[i] === "nl"){
-            currentLine = document.createElement("div");
-            currentLine.classList.add("lineStyle");
-            keyboardDiv.appendChild(currentLine);
-
-        } else{
-            const letterBox = document.createElement("div");
-            const letter = document.createElement("p");
-            letter.classList.add("letterStyle");
-            letter.textContent = keys[i];
-            letterBox.appendChild(letter);
-            letterBox.classList.add("letterBoxStyle");
-            letterBox.classList.add("BtnStyle");
-            letterBox.id = keys[i];
-            currentLine.appendChild(letterBox);
-        }
-
-    }
-}
-
-export function updateLetter(correct, letter){
-    const letters = document.querySelectorAll(".letterBoxStyle");
-    for(let i = 0; i < letters.length; i++){
-        if(letters[i].id === letter && correct){
-            letters[i].classList.add("correctLetter");
-            return;
-        } else if(letters[i].id === letter && !(correct)){
-            letters[i].classList.add("incorrectLetter");
-        }
-    }
-
-}
-
-export function toggleSettings(toggle){
-    const overlay = document.querySelector("#overlay");
-    if(toggle){
-        overlay.style.display = "flex";
+export function createKeyboard() {
+  const keys = ['nl', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'nl', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'nl', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+  const keyboardDiv = document.querySelector('#keyboard');
+  let currentLine;
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i] === 'nl') {
+      currentLine = document.createElement('div');
+      currentLine.classList.add('lineStyle');
+      keyboardDiv.appendChild(currentLine);
     } else {
-        overlay.style.display = "none";
+      const letterBox = document.createElement('div');
+      const letter = document.createElement('p');
+      letter.classList.add('letterStyle');
+      letter.textContent = keys[i];
+      letterBox.appendChild(letter);
+      letterBox.classList.add('letterBoxStyle');
+      letterBox.classList.add('BtnStyle');
+      letterBox.id = keys[i];
+      currentLine.appendChild(letterBox);
     }
+  }
 }
 
-export function updateDom(lives, livesCounter, underscores, hint, wordDef){
-    const playPage = document.querySelector("#playing");
-    const HangPic = document.querySelector("#HangPic");
-    const hintP=document.querySelector("#hintP");
-
-    playPage.classList.remove("invis");
-    wordBox.textContent = underscores.join("");
-    HangPic.src = `assets/${lives}/${lives-livesCounter}.png`;
-    hintP.textContent = "";
-    if(hint && livesCounter < lives/2){
-        hintP.textContent = `Hint: ${wordDef}`
+export function updateLetter(correct, letter) {
+  const letters = document.querySelectorAll('.letterBoxStyle');
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i].id === letter && correct) {
+      letters[i].classList.add('correctLetter');
+      return;
+    } else if (letters[i].id === letter && !(correct)) {
+      letters[i].classList.add('incorrectLetter');
     }
+  }
 }
 
-export function end(completeWord, win){
-    const endPage = document.querySelector("#end");
-    const wordBox = document.querySelector("#wordBox");
-    endPage.classList.remove("invis");
-    if (!win){
-        wordBox.textContent = completeWord;
-        wordBox.classList.add("incorrectWord");
-    } else{
-        wordBox.classList.add("correctWord");
-    }
+export function toggleSettings(toggle) {
+  const overlay = document.querySelector('#overlay');
+  if (toggle) {
+    overlay.style.display = 'flex';
+  } else {
+    overlay.style.display = 'none';
+  }
+}
 
+export function updateDom(lives, livesCounter, underscores, hint, wordDef) {
+  const playPage = document.querySelector('#playing');
+  const HangPic = document.querySelector('#HangPic');
+  const hintP = document.querySelector('#hintP');
+  const wordBox = document.querySelector('#wordBox');
+
+  playPage.classList.remove('invis');
+  wordBox.textContent = underscores.join('');
+  HangPic.src = `assets/${lives}/${lives - livesCounter}.png`;
+  hintP.textContent = '';
+  if (hint && livesCounter < lives / 2) {
+    hintP.textContent = `Hint: ${wordDef}`;
+  }
+}
+
+export function end(completeWord, win) {
+  const endPage = document.querySelector('#end');
+  const wordBox = document.querySelector('#wordBox');
+  endPage.classList.remove('invis');
+  if (!win) {
+    wordBox.textContent = completeWord;
+    wordBox.classList.add('incorrectWord');
+  } else {
+    wordBox.classList.add('correctWord');
+  }
 }
--- static/lib/hangman.mjs
+++ static/lib/hangman.mjs
@@ -1,8 +1,8 @@
-import * as wordLib from "./localWordLib.mjs"
-import * as domLib from "./domLib.mjs"
+import * as wordLib from './localWordLib.mjs';
+import * as domLib from './domLib.mjs';
 
 
-//VARS/////////////////////////////////////////////////////////////////////////////////////////////
+// VARS/////////////////////////////////////////////////////////////////////////////////////////////
 let livesCounter;
 let lives;
 let win;
@@ -11,115 +11,112 @@ let hint = false;
 let word;
 
 
-//start Functions/////////////////////////////////////////////////////////////////////////////////////
-export function init(){
-    domLib.createKeyboard();
-    makeEventListeners();
-    reset();
+// start Functions/////////////////////////////////////////////////////////////////////////////////////
+export function init() {
+  domLib.createKeyboard();
+  makeEventListeners();
+  reset();
 }
 
-function makeEventListeners(){
-    //screen keyboard listeners
-    const keyButtons = document.querySelectorAll(".letterBoxStyle");
-    for(let i = 0; i < keyButtons.length; i++){
-        keyButtons[i].addEventListener("click", (e) => {turn(keyButtons[i].id)});
+function makeEventListeners() {
+  // screen keyboard listeners
+  const keyButtons = document.querySelectorAll('.letterBoxStyle');
+  for (let i = 0; i < keyButtons.length; i++) {
+    keyButtons[i].addEventListener('click', () => { turn(keyButtons[i].id); });
+  }
+
+  // real keyboard
+  document.addEventListener('keydown', (e) => {
+    if (/[a-z]/.test(e.key) && e.key.length === 1) {
+      turn(e.key);
     }
+  });
 
-    //real keyboard
-    document.addEventListener("keydown", (e) => {
-        if(/[a-z]/.test(e.key) && e.key.length === 1){
-            turn(e.key);
-        }
-    })
+  // replay button
+  const replay = document.querySelector('#replay');
+  replay.addEventListener('click', reset);
 
-    //replay button 
-    const replay = document.querySelector("#replay");
-    replay.addEventListener("click", reset);
-
-    //settings
-    const settingsLinks = document.querySelectorAll("#settingLink");
-    const startBtn = document.querySelector("#startBtn");
-    for(let i = 0; i < settingsLinks.length; i++){
-        settingsLinks[i].addEventListener("click", () => {domLib.toggleSettings(true)});
-    }
-    startBtn.addEventListener("click", reset);
+  // settings
+  const settingsLinks = document.querySelectorAll('#settingLink');
+  const startBtn = document.querySelector('#startBtn');
+  for (let i = 0; i < settingsLinks.length; i++) {
+    settingsLinks[i].addEventListener('click', () => { domLib.toggleSettings(true); });
+  }
+  startBtn.addEventListener('click', reset);
 }
 
-function reset(){
-    const endPage = document.querySelector("#end");
-    //reset keyboard 
-    const keys = document.querySelectorAll(".letterBoxStyle");
-    for (let i = 0; i < keys.length; i++){
-        keys[i].className = "letterBoxStyle BtnStyle";
-    }
-    //reset word 
-    const wordBox = document.querySelector("#wordBox");
-    wordBox.className = "";
+function reset() {
+  const endPage = document.querySelector('#end');
+  // reset keyboard
+  const keys = document.querySelectorAll('.letterBoxStyle');
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].className = 'letterBoxStyle BtnStyle';
+  }
+  // reset word
+  const wordBox = document.querySelector('#wordBox');
+  wordBox.className = '';
 
-    win = false;
-    usedLetters = [];
-    endPage.classList.add("invis");
-    startGame();
-    
+  win = false;
+  usedLetters = [];
+  endPage.classList.add('invis');
+  startGame();
 }
 
-async function startGame(){
-    domLib.toggleSettings(false);
-    word = await wordLib.fetchWords();
-    hint = document.querySelector("#hintTrue").checked;
-    setLives();
-    domLib.updateDom(lives, livesCounter, word.underscores);
+async function startGame() {
+  domLib.toggleSettings(false);
+  word = await wordLib.fetchWords();
+  hint = document.querySelector('#hintTrue').checked;
+  setLives();
+  domLib.updateDom(lives, livesCounter, word.underscores);
 }
 
-function setLives(){
-    const elem4 = document.querySelector("#lives4").checked;
-    const elem8 = document.querySelector("#lives8").checked;
-    const elem12 = document.querySelector("#lives12").checked;
+function setLives() {
+  const elem4 = document.querySelector('#lives4').checked;
+  const elem8 = document.querySelector('#lives8').checked;
+  const elem12 = document.querySelector('#lives12').checked;
 
-    if(elem4 === true){
-        lives = 4;
-        livesCounter = 4;
-    } else if(elem8 === true){
-        lives = 8;
-        livesCounter = 8;
-    } else if(elem12 === true){
-        lives = 12;
-        livesCounter = 12;
-    }
+  if (elem4 === true) {
+    lives = 4;
+    livesCounter = 4;
+  } else if (elem8 === true) {
+    lives = 8;
+    livesCounter = 8;
+  } else if (elem12 === true) {
+    lives = 12;
+    livesCounter = 12;
+  }
 }
 
-//In Game Functions////////////////////////////////////////////////////////////////////////////////////////
-function turn(letter){
-    if(usedLetters.includes(letter)){return;}
-    if(!word){return;}
+// In Game Functions////////////////////////////////////////////////////////////////////////////////////////
+function turn(letter) {
+  if (usedLetters.includes(letter)) { return; }
+  if (!word) { return; }
 
-    info2.textContent = "";
-    usedLetters.push(letter);
-    const OldUnderscour = [];
-    for(const item of word.underscores){
-        OldUnderscour.push(item);
+  usedLetters.push(letter);
+  const OldUnderscour = [];
+  for (const item of word.underscores) {
+    OldUnderscour.push(item);
+  }
+  for (let i = 0; i < word.word.length; i++) {
+    if (word.word[i] === letter) {
+      word.underscores[i] = letter;
+      domLib.updateLetter(true, letter);
     }
-    for (let i = 0; i<word.word.length; i++){
-        if (word.word[i] === letter){
-            word.underscores[i] = letter;
-            domLib.updateLetter(true, letter);
-        }
-    }
-    if (OldUnderscour.join("") === word.underscores.join("")){
-        domLib.updateLetter(false, letter);
-        livesCounter -=1;
-    }
+  }
+  if (OldUnderscour.join('') === word.underscores.join('')) {
+    domLib.updateLetter(false, letter);
+    livesCounter -= 1;
+  }
 
-    //end of turn
-    domLib.updateDom(lives, livesCounter, word.underscores, hint, word.def);
+  // end of turn
+  domLib.updateDom(lives, livesCounter, word.underscores, hint, word.def);
 
-    win = word.underscores.join("") === word.word;
-    if(win){
-        domLib.end(word.word, win);
-        word = "";
-    } else if (livesCounter <= 0){
-        domLib.end(word.word, win);
-        word = "";
-    }
+  win = word.underscores.join('') === word.word;
+  if (win) {
+    domLib.end(word.word, win);
+    word = '';
+  } else if (livesCounter <= 0) {
+    domLib.end(word.word, win);
+    word = '';
+  }
 }
-
