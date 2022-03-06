7462f6e90c6b1b69fa4574c059f49b5c98e67597
howlingunit <tyler_pitt@outlook.com>
2022-03-06 13:48:21 +0000

    added check

--- svr.js
+++ svr.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { pickWord } from './lib/wordLib.mjs';
+import * as word from './lib/wordLib.mjs';
 
 const app = express();
 
 app.use(express.static('static'));
 
-app.post('/getWord', express.json(), pickWord);
+app.post('/getWord', express.json(), word.pickWord);
+
+app.post('/submitWord', express.json, word.submitWord);
 
 app.listen(8081);
 
