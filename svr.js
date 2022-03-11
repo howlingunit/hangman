1989671771f6a6662f43cec58a09be369ee4ffc0
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-03-11 13:21:22 +0000

    bug fixes

--- svr.js
+++ svr.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import * as word from './lib/wordLib.mjs';
+import * as sql from './lib/SqlInit.mjs';
 
 const app = express();
 
 app.use(express.static('static'));
 
-app.get('/word/:random.:movie.:tvshow.:user', (req, res) => {
-  console.log(req.params);
-  res.send(req.params);
-});
+app.get('/word/:random.:movie.:tvshow.:user', word.pickWord);
+
 app.post('/getWord', express.json(), word.pickWord);
 
-app.post('/submitWord', express.json, word.submitWord);
 
 app.listen(8081);
 
+// console.log(await sql.listWords());
 // sql.pushWords();
