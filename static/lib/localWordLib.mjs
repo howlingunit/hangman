d34ec0056039d3893eaf79bab0a8d0c9661bb6f0
howlingunit <tyler_pitt@outlook.com>
2022-03-25 19:26:56 +0000

    url fix (wip)

--- static/lib/localWordLib.mjs
+++ static/lib/localWordLib.mjs
@@ -6,7 +6,14 @@ export async function fetchWords() {
   const elemTVShows = document.querySelector('#TV-shows');
   const elemUsers = document.querySelector('#userSubmited');
 
-  const urlPrams = `words=${elemRandomWords.checked},${elemMovies.checked}&tvshow=${elemTVShows.checked}&user=${elemUsers.checked}`;
+  const wordTypes = [];
+
+  if (elemRandomWords.checked) { wordTypes.push('random'); }
+  if (elemMovies.checked) { wordTypes.push('movie'); }
+  if (elemTVShows.checked) { wordTypes.push('tvshow'); }
+  if (elemUsers.checked) { wordTypes.push('user'); }
+
+  const urlPrams = `words=${wordTypes.join(',')}`;
 
   const response = await fetch(`word/?${urlPrams}`);
 
